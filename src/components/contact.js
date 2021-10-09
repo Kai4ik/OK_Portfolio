43e6a2c3f24c3c75952db28e9d2e06e4f9aac648
Kairat <orozobekov.kai@gmail.com>
2021-10-09 04:42:53 -0400

    contact form - scrolToNext form field added

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useForm } from "@formspree/react";
 import * as Scroll from "react-scroll";
-import $ from "jquery";
 
 const ContactSection = styled.div`
   display: flex;
