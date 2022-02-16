16aaed6d900408e94006e93608921be3107243a9
Kairat <orozobekov.kai@gmail.com>
2022-02-16 02:51:44 -0500

    Images moved to Firebase Storage

--- src/components/projects-data.js
+++ src/components/projects-data.js
@@ -1,14 +1,3 @@
-import thc_desktop from "../images/thc_desktop.png";
-import thc_mobile from "../images/thc_mobile.png";
-import sma_desktop from "../images/sma_desktop.png";
-import sma_mobile from "../images/sma_mobile.png";
-import omnifood_desktop from "../images/omnifood_desktop.png";
-import omnifood_mobile from "../images/omnifood_mobile.png";
-import shoppingify_mobile from "../images/shoppingify_mobile.png";
-import shoppingify_desktop from "../images/shoppingify_desktop.png";
-import wildfire_desktop from "../images/wildfire_desktop.png";
-import wildfire_mobile from "../images/wildfire_mobile.png";
-
 const projectsData = [
   {
     id: "ee99d6ff-a70f-4026-bf13-1b120972fd54",
@@ -31,8 +20,10 @@ const projectsData = [
       "jQuery",
       "bcrypt",
     ],
-    imageMobile: thc_mobile,
-    imageDesktop: thc_desktop,
+    imageMobile:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/thc_mobile.png?alt=media&token=df1fcbd7-8036-43a6-af2d-f060a847e40f",
+    imageDesktop:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/thc_desktop.png?alt=media&token=338a5f85-174c-4dce-b748-493cb8c8f559",
   },
   {
     id: "6194d233-cb1f-453a-9586-e0708eaff662",
@@ -54,8 +45,10 @@ const projectsData = [
       "Sequelize",
       "Recharts",
     ],
-    imageMobile: shoppingify_mobile,
-    imageDesktop: shoppingify_desktop,
+    imageMobile:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/shoppingify_mobile.png?alt=media&token=a789ead5-24e2-4a14-8e1f-6401e4d7b4e7",
+    imageDesktop:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/shoppingify_desktop.png?alt=media&token=4c6459b1-db5c-49dc-b8d8-b637d82b8d81",
   },
   {
     id: "b08cb9f3-8648-405d-b2bc-30a00776a04e",
@@ -71,8 +64,10 @@ const projectsData = [
       "NASA API",
       "Google Maps API",
     ],
-    imageMobile: wildfire_mobile,
-    imageDesktop: wildfire_desktop,
+    imageMobile:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/wildfire_mobile.png?alt=media&token=9c4bb095-11eb-4bab-a175-c9d25b73e76e",
+    imageDesktop:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/wildfire_desktop.png?alt=media&token=3ecf3ca7-f3b1-46ed-8638-325bf4475599",
   },
   {
     id: "6481af5a-4ad8-44ab-8cdc-46ffc56189bf",
@@ -82,8 +77,10 @@ const projectsData = [
     description:
       "Simple web application build using React and styled-components. The entire project utilized the concept of React Hooks. The app allows users to play track, play next/previous one, or choose a particular track",
     technologiesUsed: ["React", "styled-components", "CSS", "HTML5 "],
-    imageMobile: sma_mobile,
-    imageDesktop: sma_desktop,
+    imageMobile:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/sma_mobile.png?alt=media&token=45a86ecd-45fd-43d7-b8af-d5bfe396a12a",
+    imageDesktop:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/sma_desktop.png?alt=media&token=a6135956-4c2f-4c4f-8a50-8b26b5655b55",
   },
   {
     id: "374f5d0e-2427-4d72-b7a8-d066a92a5091",
@@ -93,8 +90,10 @@ const projectsData = [
     description:
       "Omnifood – simple responsive website built with HTML5, plain CSS3, vanilla JavaScript, and jQuery. All DOM manipulation is done with the jQuery library. Waypoints library is used to trigger animations (bounce up, pulse, fade in) when scrolling to a particular element. Responsiveness of the website was achieved by the usage of Media Queries.",
     technologiesUsed: ["HTML5", "CSS3", "jQuery", "Waypoints", "JavaScript"],
-    imageMobile: omnifood_mobile,
-    imageDesktop: omnifood_desktop,
+    imageMobile:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/omnifood_mobile.png?alt=media&token=7c3ef0a1-9180-42e0-bd60-082f27040ad5",
+    imageDesktop:
+      "https://firebasestorage.googleapis.com/v0/b/ok-portfolio.appspot.com/o/omnifood_desktop.png?alt=media&token=b86487bd-7726-42d9-9e7f-4899dd6f5f6c",
   },
 ];
 
